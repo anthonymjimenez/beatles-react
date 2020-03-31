3b6c313ffa97e49c8433bf3b013a6237517d9b76
securethebags <anthonyjim96@gmail.com>
2020-03-31 19:31:18 -0400

    initial CSS step up complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { YellowSub } from './pages/YellowSub'
 import { AbbeyRoad } from './pages/AbbeyRoad';
 import { Revolver } from './pages/Revolver';
+import './Global.css'
 function App() {
   return (
     <div className="App">
--- src/template/Page.js
+++ src/template/Page.js
@@ -3,21 +3,20 @@ import ReadButton from '../components/style/readButton'
 import BuyButton from '../components/style/buyButton'
 import Quote from '../components/style/quote'
 import Table from '../components/style/table'
-import { css, jsx } from '@emotion/core'
+import './Page.css'
 //css margins etc ....general CSS 
 
 export const Page = ({ title, quote, body, table, buyMore, readMore}) => {
     return (
         <>
-        <div
-    css={css`font-size: 50px;`} >
-        </div>
+         <div className = "page">
             <h1>{title}</h1>
             <Quote>{quote()}</Quote>
             <ReadButton link={readMore}>Read more</ReadButton>
             <p>{body()}</p>
             <Table>{table()}</Table> 
             <BuyButton link = {buyMore}>Buy more</BuyButton>
+            </div>
         </>
     )
 }
